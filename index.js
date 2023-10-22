cb785df0500e63985696c158ac99f8b8406814e6
Nibinh <haridasnibin@gmail.com>
2023-10-22 08:52:47 +0530

    envFile

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const server = express();
 
 server.use(
   cors({
-    origin: "https://mindfull-client.onrender.com",
+    // origin: "https://mindfull-client.onrender.com",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
